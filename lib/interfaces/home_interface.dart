a4e5f3727f6fd3957b37d78dffdf8b3571d13c44
WaleedK2000 <waleedkayani14@yahoo.com>
2022-05-23 22:11:31 +0500

    code refactor and homescreen remodel

---
+++ lib/interfaces/home_interface.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class home_interface {
+  getMenuItems();
+  menuItemSelected(item, BuildContext context);
+}
---
+++ lib/screens/home/oldHome.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../data_screens/player_list.dart';
+import '../new_tournament/tournament_add.dart';
+import '../team/TeamsListScreen.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.amber[400],
+              title: const Text('Home Screen'),
+              elevation: 0.0,
+            ),
+            body: Container(
+                child: Form(
+                    child: Column(
+              children: <Widget>[
+                _buildNewTournamentButton(context),
+                _buildViewPlayers(context),
+                _buildViewTeams(context)
+              ],
+            )))));
+  }
+
+  Widget _buildNewTournamentButton(BuildContext context) => ElevatedButton(
+        child: const Text('Add Tournament'),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const NewTournament()),
+          );
+        },
+      );
+
+  Widget _buildViewPlayers(BuildContext context) => ElevatedButton(
+        child: const Text('Player Stats'),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const PlayerListScreen()),
+          );
+        },
+      );
+
+  Widget _buildViewTeams(BuildContext context) => ElevatedButton(
+        child: const Text('Teams'),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const TeamsListScreen()),
+          );
+        },
+      );
+}
